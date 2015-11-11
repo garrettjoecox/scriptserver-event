5b1f113b7fdb8f796831414ef69e443e2326a917
Garrett Cox <garrettjcox@gmail.com>
2015-11-11 10:21:04 -0600

    Optimize regex, add achievement event & update dependencies

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 var ScriptServer = require('scriptserver');
 
 module.exports = function(server) {
-    
+
     server.events = server.events || {};
-    
+
     server.parseLoop.parseConsole = {
         id: 'parseConsole',
         condition: function() { return server.events.console; },
@@ -12,11 +12,11 @@ module.exports = function(server) {
             if (server.events.console) server.events.console(line);
         }
     };
-    
+
     server.parseLoop.parseChat = {
         id: 'parseChat',
         condition: function() { return server.events.chat; },
-        regexp: /<([\S]+)>\s(.*)/,
+        regexp: /\]:\s<([\w]+)>\s(.*)/,
         method: function(stripped) {
             if (stripped && server.events.chat) server.events.chat({
                 sender: stripped[1],
@@ -25,11 +25,11 @@ module.exports = function(server) {
             });
         }
     };
-    
+
     server.parseLoop.parseLogin = {
         id: 'parseLogin',
         condition: function() { return server.events.login; },
-        regexp: /\]:\s(\S+)\[\/((?:[0-9]{1,3}\.){3}[0-9]{1,3}).*at\s\((-?[\d]+\.[\d]+), (-?[\d]+\.[\d]+), (-?[\d]+\.[\d]+)/,
+        regexp: /]:\s([\w]+)\[\/([.\d]+)[^(]+\(([-.\d]+),\s([-.\d]+),\s([-.\d]+)/,
         method: function(stripped) {
             if (stripped && server.events.login) server.events.login({
                 player: stripped[1],
@@ -41,11 +41,11 @@ module.exports = function(server) {
             });
         }
     };
-    
+
     server.parseLoop.parseLogout = {
         id: 'parseLogout',
         condition: function() { return server.events.logout; },
-        regexp: /\]:\s(\S+).*text='(.*)',\ssiblings/,
+        regexp: /]:\s([\w]+)[^{]+{text='([\w ]+)'/,
         method: function(stripped) {
             if (stripped && server.events.logout) server.events.logout({
                 player: stripped[1],
@@ -54,7 +54,21 @@ module.exports = function(server) {
             });
         }
     };
-    
+
+    server.parseLoop.parseAchievement = {
+        id: 'parseAchievement',
+        condition: function() { return server.events.achievement; },
+        regexp: /]:\s([\w]+)[^[]+\[([\w ]+)\]/,
+        method: function(stripped) {
+            console.log(stripped);
+            if (stripped && server.events.achievement) server.events.achievement({
+                player: stripped[1],
+                achievement: stripped[2],
+                timestamp: Date.now()
+            });
+        }
+    };
+
 };
 
 ScriptServer.prototype.on = function(eventName, callback) {
