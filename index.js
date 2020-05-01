406d3ca7a18b1bd51dfdb7f866a51848ba93cdad
Garrett Cox <garrettjcox@gmail.com>
2020-04-30 21:23:47 -0500

    1.1.1 Release

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ module.exports = function () {
           }
         },
         parseAchievementEvent(string) {
-          const parsed = string.match(/^\[[\d:]{8}\] \[Server thread\/INFO\]: (\w+) has made the advancement \[([\w\s]+)\]/);
+          const parsed = string.match(/^\[[\d:]{8}\] \[Server thread\/INFO\]: (\w+) has completed the challenge \[([\w\s]+)\]/);
           if (parsed) {
             return {
               player: parsed[1],
@@ -50,150 +50,6 @@ module.exports = function () {
         },
         parseStopEvent(string) {
           const parsed = string.match(/^\[[\d:]{8}\] \[Server thread\/INFO\]: Stopping server/);
-          if (parsed) {
-            return {};
-          }
-        },
-      },
-      spigot: {
-        parseChatEvent(string) {
-          const parsed = string.match(/^\[[\d:]{8} INFO\]: <(\w+)> (.*)/i);
-          if (parsed) {
-            return {
-              player: parsed[1],
-              message: parsed[2],
-            };
-          }
-        },
-        parseLoginEvent(string) {
-          const parsed = string.match(/^\[[\d:]{8} INFO\]: (\w+)\[\/([\d.:]+)\] logged in/);
-          if (parsed) {
-            return {
-              player: parsed[1],
-              ip: parsed[2],
-            };
-          }
-        },
-        parseLogoutEvent(string) {
-          const parsed = string.match(/^\[[\d:]{8} INFO\]: (\w+) lost connection/);
-          if (parsed) {
-            return {
-              player: parsed[1],
-            };
-          }
-        },
-        parseAchievementEvent(string) {
-          const parsed = string.match(/^\[[\d:]{8} INFO\]: (\w+) has made the advancement \[([\w\s]+)\]/);
-          if (parsed) {
-            return {
-              player: parsed[1],
-            };
-          }
-        },
-        parseStartEvent(string) {
-          const parsed = string.match(/^\[[\d:]{8} INFO\]: Done/);
-          if (parsed) {
-            return {};
-          }
-        },
-        parseStopEvent(string) {
-          const parsed = string.match(/^\[[\d:]{8} INFO\]: Stopping server/);
-          if (parsed) {
-            return {};
-          }
-        },
-      },
-      sponge: {
-        parseChatEvent(string) {
-          const parsed = string.match(/^\[[\d:]{8} INFO\]: <(\w+)> (.*)/i);
-          if (parsed) {
-            return {
-              player: parsed[1],
-              message: parsed[2],
-            };
-          }
-        },
-        parseLoginEvent(string) {
-          const parsed = string.match(/^\[[\d:]{8} INFO\]: (\w+)\[\/([\d.:]+)\] logged in/);
-          if (parsed) {
-            return {
-              player: parsed[1],
-              ip: parsed[2],
-            };
-          }
-        },
-        parseLogoutEvent(string) {
-          const parsed = string.match(/^\[[\d:]{8} INFO\]: (\w+) lost connection/);
-          if (parsed) {
-            return {
-              player: parsed[1],
-            };
-          }
-        },
-        parseAchievementEvent(string) {
-          const parsed = string.match(/^\[[\d:]{8} INFO\]: (\w+) has made the advancement \[([\w\s]+)\]/);
-          if (parsed) {
-            return {
-              player: parsed[1],
-            };
-          }
-        },
-        parseStartEvent(string) {
-          const parsed = string.match(/^\[[\d:]{8} INFO\]: Done/);
-          if (parsed) {
-            return {};
-          }
-        },
-        parseStopEvent(string) {
-          const parsed = string.match(/^\[[\d:]{8} INFO\]: Stopping server/);
-          if (parsed) {
-            return {};
-          }
-        },
-      },
-      glowstone: {
-        parseChatEvent(string) {
-          const parsed = string.match(/^[\d:]{8} \[INFO\] <(\w+)> (.*)/i);
-          if (parsed) {
-            return {
-              player: parsed[1],
-              message: parsed[2],
-            };
-          }
-        },
-        parseLoginEvent(string) {
-          const parsed = string.match(/^[\d:]{8} \[INFO\] (\w+) \[\/([\d.:]+)\] connected/);
-          if (parsed) {
-            return {
-              player: parsed[1],
-              ip: parsed[2],
-            };
-          }
-        },
-        parseLogoutEvent(string) {
-          const parsed = string.match(/^[\d:]{8} \[INFO\] (\w+) \[\/([\d.:]+)\] lost connection/);
-          if (parsed) {
-            return {
-              player: parsed[1],
-            };
-          }
-        },
-        parseAchievementEvent(string) {
-          const parsed = string.match(/^[\d:]{8} \[INFO\] (\w+) has made the advancement \[([\w\s]+)\]/);
-          if (parsed) {
-            return {
-              player: parsed[1],
-            };
-          }
-        },
-        parseStartEvent(string) {
-          const parsed = string.match(/^[\d:]{8} \[INFO\] Ready for connections/);
-          if (parsed) {
-            return {};
-          }
-        },
-        parseStopEvent(string) {
-          const parsed = string.match(/^[\d:]{8} \[INFO\] The server is shutting down/);
           if (parsed) {
             return {};
           }
@@ -215,7 +71,7 @@ module.exports = function () {
     const result = events.reduce((acc, event) => {
       if (acc) return acc;
 
-      const parseEvent = get(server.config.event, `flavorSpecific.${server.config.flavor}.${event[1]}`, server.config.event.flavorSpecific.default[event[1]]);
+      const parseEvent = get(server.config.event, ['flavorSpecific', server.config.flavor, event[1]], server.config.event.flavorSpecific.default[event[1]]);
       const matches = parseEvent(string);
       if (matches) return { event: event[0], payload: matches };
 
